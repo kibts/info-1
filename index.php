11cc36206f83d12fad9a13d8f49502cbd6faf264
kibts <124083041+kibts@users.noreply.github.com>
2023-03-04 14:24:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5VWVGVS');</script>
+})(window,document,'script','dataLayer','GTM-WGKBCWV');</script>
 <!-- End Google Tag Manager -->
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VWVGVS"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGKBCWV"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
